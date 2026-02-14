01c92e5a2d5cbdc3ea9a21957584a684759d0500
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2026-02-14 22:25:39 +0530

    Updated Code

--- frontend/src/hooks/useSubscribe/api.ts
+++ frontend/src/hooks/useSubscribe/api.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 async function createSubscription() {
   const baseUrl = import.meta.env.VITE_BASE_BACKEND_URL
   const res = await axios.get(`${baseUrl}/api/payment/checkout-payment`);
-  
   return res.data
 }
 export default createSubscription;
